8df5aaca7df8e750664b5065f067ac86b672832e
julycabigas <july@businessblueprint.com>
2018-09-13 06:59:09 +0800

    update edit membership info

--- public/partials/edit-membership-info.php
+++ public/partials/edit-membership-info.php
@@ -16,11 +16,11 @@
 			   	  <input type="text" name="firstname" value="<?php echo $this->firstname; ?>" style="color: #bfbcbc;" readonly="readonly"   class="form-control required-field"  required/>
 			   </div>
 			   <div class="col-sm-6">
-			      <label>Last Name</label> <span class="privacy-status <?php echo ( $is_silver ) ? 'hide': ''; ?>""><input type="checkbox" name="hide_lastname" <?php echo ( get_user_meta(  $user_id , 'hide_lastname', true ) == 1 ) ? "checked='checked'": ""; ?> /> Hide</span><br>
+			      <label>Last Name</label> <span class="privacy-status <?php echo ( $is_silver ) ? 'hide': ''; ?>"><input type="checkbox" name="hide_lastname" <?php echo ( get_user_meta(  $user_id , 'hide_lastname', true ) == 1 ) ? "checked='checked'": ""; ?> /> Hide</span><br>
 			   	  <input type="text" name="lastname" value="<?php echo $this->lastname; ?>" style="color: #bfbcbc";  class="form-control  required-field" readonly="readonly" required/>
 			   </div>
 			   <div class="col-sm-6">
-			      <label>Address</label> <span class="privacy-status <?php echo ( $is_silver ) ? 'hide': ''; ?>""><input type="checkbox" name="show_address" <?php echo ( !get_user_meta(  $user_id , 'show_address', true ) == 1 ) ? "checked='checked'": ""; ?>> Hide Complete Address</span><br>
+			      <label>Address</label> <span class="privacy-status <?php echo ( $is_silver ) ? 'hide': ''; ?>"><input type="checkbox" name="show_address" <?php echo ( !get_user_meta(  $user_id , 'show_address', true ) == 1 ) ? "checked='checked'": ""; ?>> Hide Complete Address</span><br>
 			   	  <input type="text" name="address" value="<?php echo $this->address; ?>"  class="form-control  required-field" required/>
 			   </div>
 			   <div class="col-sm-6">
@@ -31,8 +31,8 @@
 			      <label>City</label><br>
 			   	  <input type="text" name="city" value="<?php echo $this->city; ?>" class="form-control  required-field" required/>
 			   </div>
-			   <div class="col-sm-6">
-			      <label>State</label><span class="privacy-status"><input type="checkbox" name="hide_state" <?php echo ( get_user_meta(  $user_id , 'hide_state', true ) == 1 ) ? "checked='checked'": ""; ?> /> Hide</span><br>
+			   <div class="col-sm-6"> 
+			      <label>State</label><span class="privacy-status <?php echo ( $is_silver ) ? 'hide': ''; ?>"><input type="checkbox" name="hide_state" <?php echo ( get_user_meta(  $user_id , 'hide_state', true ) == 1 ) ? "checked='checked'": ""; ?> /> Hide</span><br>
 			      <?php echo $this->getOptions( $this->state_edit, 'form-control  required-field', 'state', 'states' ); ?> 
 			   </div>
 				<div class="col-sm-6">
@@ -40,11 +40,11 @@
 			   	  <input type="text" name="zip" value="<?php echo $this->zipcode; ?>" class="form-control  required-field" required/>
 			   </div>
 			   <div class="col-sm-6">
-			      <label>Mobile No</label> <span class="privacy-status <?php echo ( $is_silver ) ? 'hide': ''; ?>""><input type="checkbox" name="show_mobile" <?php echo ( !get_user_meta(  $user_id , 'show_mobile', true ) == 1 ) ? "checked='checked'": ""; ?> /> Hide</span><br>
+			      <label>Mobile No</label> <span class="privacy-status <?php echo ( $is_silver ) ? 'hide': ''; ?>"><input type="checkbox" name="show_mobile" <?php echo ( !get_user_meta(  $user_id , 'show_mobile', true ) == 1 ) ? "checked='checked'": ""; ?> /> Hide</span><br>
 			   	  <input type="text" name="mobile_no" value="<?php echo $this->mobile_no; ?>" class="form-control  required-field" required/>
 			   </div>
 			   <div class="col-sm-6">
-			      <label>Email</label> <span class="privacy-status <?php echo ( $is_silver ) ? 'hide': ''; ?>""><input type="checkbox" name="show_email" <?php echo ( !get_user_meta(  $user_id , 'show_email', true ) == 1 ) ? "checked='checked'": ""; ?> /> Hide</span><br>
+			      <label>Email</label> <span class="privacy-status <?php echo ( $is_silver ) ? 'hide': ''; ?>"><input type="checkbox" name="show_email" <?php echo ( !get_user_meta(  $user_id , 'show_email', true ) == 1 ) ? "checked='checked'": ""; ?> /> Hide</span><br>
 			   	  <input type="email" name="email" value="<?php echo $this->email; ?>" class="form-control  required-field" disabled/>
 			   </div>
 		    </div>
@@ -53,19 +53,19 @@
 		    <div class="row">
 			   <div class="col-sm-6">
 			      <label>Company</label> 
-			      <span class="privacy-status <?php echo ( $is_silver ) ? 'hide': ''; ?>""><input type="checkbox" name="show_company" <?php echo ( !get_user_meta(  $user_id , 'show_company', true ) == 1 ) ? "checked='checked'": ""; ?>  /> Hide</span><br>
+			      <span class="privacy-status <?php echo ( $is_silver ) ? 'hide': ''; ?>"><input type="checkbox" name="show_company" <?php echo ( !get_user_meta(  $user_id , 'show_company', true ) == 1 ) ? "checked='checked'": ""; ?>  /> Hide</span><br>
 			   	  <input type="text" name="company" value="<?php echo $this->company; ?>" class="form-control" />
 			   </div>
 			   <div class="col-sm-6">
-			      <label>Website</label> <span class="privacy-status"><input type="checkbox" name="show_website" <?php echo ( !get_user_meta(  $user_id , 'show_website', true ) == 1 ) ? "checked='checked'": ""; ?>  /> Hide</span><br>
+			      <label>Website</label> <span class="privacy-status <?php echo ( $is_silver ) ? 'hide': ''; ?>"><input type="checkbox" name="show_website" <?php echo ( !get_user_meta(  $user_id , 'show_website', true ) == 1 ) ? "checked='checked'": ""; ?>  /> Hide</span><br>
 			   	  <input type="text" name="website" value="<?php echo $this->website; ?>" class="form-control" />
 			   </div>
 			   <div class="col-sm-6">
-			      <label>Office No.</label> <span class="privacy-status"><input type="checkbox" name="show_office_no" <?php echo ( !get_user_meta(  $user_id , 'show_office_no', true ) == 1 ) ? "checked='checked'": ""; ?>  /> Hide</span><br>
+			      <label>Office No.</label> <span class="privacy-status <?php echo ( $is_silver ) ? 'hide': ''; ?>"><input type="checkbox" name="show_office_no" <?php echo ( !get_user_meta(  $user_id , 'show_office_no', true ) == 1 ) ? "checked='checked'": ""; ?>  /> Hide</span><br>
 			   	  <input type="text" name="office_no" value="<?php echo $this->office_no; ?>" class="form-control" />
 			   </div>
 			   		   <div class="col-sm-6" id="businessType">
-			      <label>Business Type</label> <span class="privacy-status"><input type="checkbox" name="show_business_type" <?php echo ( !get_user_meta(  $user_id , 'show_business_type', true ) == 1 ) ? "checked='checked'": ""; ?>  /> Hide</span><br>
+			      <label>Business Type</label> <span class="privacy-status <?php echo ( $is_silver ) ? 'hide': ''; ?>"><input type="checkbox" name="show_business_type" <?php echo ( !get_user_meta(  $user_id , 'show_business_type', true ) == 1 ) ? "checked='checked'": ""; ?>  /> Hide</span><br>
 			      <input type="hidden" name="business_category" id="businessTypeCategory" value="<?php echo ( get_user_meta(  $user_id , 'business_category', true ) ) ? get_user_meta(  $user_id , 'business_category', true ) : '' ?>"  required/>
 			      <?php 
 
@@ -107,31 +107,6 @@
 			        	<option value="<?php echo $type; ?>" <?php echo ( get_user_meta(  $user_id , 'business_category', true ) ) ? 'selected' : ''; ?>><?php echo $type; ?></option>
 			    		<?php } ?>
 			    	</select>	
-			          <!-- <div class="classificationWrapper"> 
-				          <div class="classficationDropDownList">
-				          	<span class="type"><?php //echo ( get_user_meta(  $user_id , 'business_category', true ) ) ? get_user_meta(  $user_id , 'business_category', true ) : 'Select classification' ?></span><i class="fa fa-chevron-down"></i>
-				          </div>		
-				          <button id="classificationClose"><span class="fa fa-close"></span></button>
-					  </div> 	
-					  <div id="classificationPanel">
-					  	<nav role="navigation">
-
-					  		<ul class="classificationMain">
-					  			<?php 
-					  			//foreach($types as $value ) : ?>
-						  			<li class="parent-item">
-						  				<a href="" role="checkbox" aria-checked="false" target="_self" <?php //echo ( get_user_meta(  $user_id , '//business_category', true ) == $value ) ? "class='is-checked is-active'": ""; ?>>
-						  					<span class="item-text">
-						  						<span><?php //echo $value ?></span>
-						  					</span>
-						  				</a>
-
-						  			</li>
-								<?php 
-								//endforeach ?>	
-					  		</ul>
-					  	</nav>
-					  </div> -->	
 			   </div>
 			</div>
 
